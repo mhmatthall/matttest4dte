c9a40e8d2aa00a7aadb89e94176941a942d7d4b7
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-06-08 18:36:25 +0100

    Add iron-session support files for authentication

---
+++ lib/withSession.jsx
@@ -0,0 +1,11 @@
+// Wrapper functions for checking session auth
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
+import { ironOptions } from "./iron-config";
+
+export function withSessionRoute(handler) {
+  return withIronSessionApiRoute(handler, ironOptions);
+}
+
+export function withSessionSsr(handler) {
+  return withIronSessionSsr(handler, ironOptions);
+}
