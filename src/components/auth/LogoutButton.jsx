318dac6135aa8ad50dd10c08ed1335b387caaaec
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-06-08 19:23:58 +0100

    Updated LogoutButton component to use axios

--- src/components/auth/LogoutButton.jsx
+++ src/components/auth/LogoutButton.jsx
@@ -1,19 +1,22 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 
 export default function LogoutButton() {
   const router = useRouter();
 
   const handleClick = async () => {
-    const res = await fetch("/api/logout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (res.ok) {
-      router.push("/");
-    }
+    axios
+      .post("/api/logout")
+      .then((res) => {
+        // Redirect to the home page if logout successful
+        if (res.status === 200) {
+          router.push("/");
+        }
+      })
+      .catch((err) => {
+        // Show an error message on the form if the login was unsuccessful
+        alert(err);
+      });
   };
 
   return (
