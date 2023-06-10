ec1c72f4c7bc01b4993217bca7d4b14127753948
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-06-10 03:38:37 +0100

    Rewrite dashboard as proper function component

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,22 +4,22 @@ import Head from "next/head";
 import { withSessionSsr } from "../../lib/session/withSession";
 
 // Actual page component
-const Home = ({ user }) => (
-  <Layout>
-    <Head>
-      <title>{"Home" + pageTitleSuffix}</title>
-    </Head>
-    <h1>Welcome back, {user.name}</h1>
-    <LogoutButton />
-  </Layout>
-);
+export default function Dashboard({ user }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{"Home" + pageTitleSuffix}</title>
+      </Head>
+      <h1>Welcome back, {user.name}</h1>
+      <LogoutButton />
+    </Layout>
+  );
+}
 
 // Server-side function to pass user data from session cookie to page props
-export const getServerSideProps = withSessionSsr(async ({ req }) => {
-  const user = req.session.user;
+export const getServerSideProps = withSessionSsr(async function ({ req }) {
+  const { user } = req.session;
   return {
     props: { user },
   };
 });
-
-export default Home;
