39325ee3be426919f795af15daf38f1a7f5fd38b
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-06-15 00:35:04 +0100

    Update example dashboard with more user data

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,9 +1,15 @@
+/**
+ * @file User dashboard page
+ */
 import LogoutButton from "@/components/auth/LogoutButton";
 import Layout, { pageTitleSuffix } from "@/components/layout/Layout";
 import Head from "next/head";
 import { withSessionSsr } from "../../lib/session/withSession";
 
-// Actual page component
+/**
+ * Dashboard page component
+ * @param {object} user The user object from the session cookie
+ */
 export default function Dashboard({ user }) {
   return (
     <Layout>
@@ -11,12 +17,27 @@ export default function Dashboard({ user }) {
         <title>{"Home" + pageTitleSuffix}</title>
       </Head>
       <h1>Welcome back, {user.name}</h1>
+      <p>
+        Your username is <code>{user.username}</code>, your UID is{" "}
+        <code>{user.userId}</code>, and you belong to the following roles:
+      </p>
+      <ul>
+        {user.roles.map((role) => (
+          <li key={role}>
+            <code>{role}</code>
+          </li>
+        ))}
+      </ul>
       <LogoutButton />
     </Layout>
   );
 }
 
-// Server-side function to pass user data from session cookie to page props
+/**
+ * Server-side function to pass user data from session cookie to page props
+ * @param {object} req The request object
+ * @returns {Promise<GetServerSidePropsResult>} The user object from the session cookie
+ */
 export const getServerSideProps = withSessionSsr(
   async function getServerSideProps({ req }) {
     const { user } = req.session;
