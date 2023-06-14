c5f48de349d21981eedb8bdb2ff5f7dd8809a508
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-06-15 00:15:09 +0100

    Protect /api routes with middleware authentication Excludes `/api/login`, so the user can always try to login

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,25 @@
+/**
+ * @file Middleware for all routes to handle authentication and authorisation.
+ */
 import { getIronSession } from "iron-session/edge";
 import { NextResponse } from "next/server";
 import { validateToken } from "../lib/auth/token";
 
+/**
+ * Middleware logic for all matching routes.
+ * @param {NextRequest} req The incoming request object
+ * @returns {NextResponse} The response object
+ */
 export const middleware = async (req) => {
   // Get NextResponse object to pass into iron-session
   const res = NextResponse.next();
 
-  // If the client is prefetching (only happens in production) then skip the middleware
+  // If the client is prefetching then skip the middleware (this only happens in production)
   if (req.headers.get("x-middleware-prefetch")) {
     return res;
   }
 
-  // We have to manually get the session cookie here rather than use our withSession wrapper
-  // because we need the iron-session/edge implementation for the middleware
+  // Edge-specific iron-session import required on middleware (we can't use our `withSession.js` functions here)
   const session = await getIronSession(req, res, {
     cookieName: "DTE-TEST_SESSION_COOKIE",
     password: process.env.DTE_TEST_SESSION_SECRET,
@@ -24,7 +31,26 @@ export const middleware = async (req) => {
   // Get user object from local session
   const { user } = session;
 
-  // If user wants /login
+  // Deny access to /api routes if user is not logged in (except /api/login, so they can log in)
+  if (
+    req.nextUrl.pathname.startsWith("/api") &&
+    req.nextUrl.pathname !== "/api/login"
+  ) {
+    if (
+      !user ||
+      !user.userId ||
+      !user.token ||
+      !validateToken(user.userId, user.token)
+    ) {
+      // If user is not logged in, then deny request
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    } else {
+      // If user is logged in, then continue to route
+      return res;
+    }
+  }
+
+  // Deny access to /login route if user is already logged in
   if (req.nextUrl.pathname === "/login") {
     if (
       user &&
@@ -43,7 +69,7 @@ export const middleware = async (req) => {
     }
   }
 
-  // If user wants /dashboard
+  // Deny access to /dashboard route if user is not logged in
   if (req.nextUrl.pathname === "/dashboard") {
     if (
       !user ||
