c73f8a58f7b266ac86f03a7ec51d359868314a41
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-08-08 00:18:16 +0100

    Migrate to Next-optimised fonts - Removed local font files - Now using next/font - Main typeface is still Inter

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,18 @@
 // Applies global styles to entire app
 import "@/styles/globals.scss";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
