6560d0eaca854894121f3d714486ab5f50005f3e
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-08-09 18:38:57 +0100

    Restyle landing page for M3

--- src/components/landing/Layout.jsx
+++ src/components/landing/Layout.jsx
@@ -1,24 +1,16 @@
 import Head from "next/head";
-import Header from "./Header";
-
 import style from "./Layout.module.scss";
-
-// Global variables
-export const pageTitleSuffix = " | DtE test";
 
 // Here comes the layout!
 export default function Layout({ children }) {
   return (
     <div className={style.root}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#FFFFFF" />
-        <meta name="author" content="CHIPS" />
+        <meta name="theme-color" content="#fff8f5" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/img/dte250.png" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <Header />
       <div className={style.main}>{children}</div>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,16 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Layout from "@/components/landing/Layout";
+import Placeholder from "@/components/landing/Placeholder";
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>DtE Test App</title>
+        <title>cronicl – Let&apos;s explore our local outdoors together.</title>
       </Head>
       <main>
-        <h1>Down to Earth x EPIC CDT</h1>
-        <p>
-          This is a next.js site hosted on Github and served by AWS Amplify!
-        </p>
-        <p>
-          <a href="/dashboard">Go to Dashboard</a>
-        </p>
-        <Image
-          src="/img/jones.png"
-          alt="Matt Jones' noggin"
-          width={500}
-          height={500}
-          priority
-        />
+        <Placeholder />
       </main>
     </Layout>
   );
