66c3062370a60328cfaaa3e9deb9bb9ff686e43a
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-08-14 00:00:53 +0100

    Fix NavButton not highlighting on child routes

--- src/components/dashboard/NavButton.jsx
+++ src/components/dashboard/NavButton.jsx
@@ -2,18 +2,19 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import style from "./NavButton.module.scss";
 
-export default function RailButton({ href, label, glyph }) {
-  // Determine if this button is active
+export default function NavButton({ href, label, glyph }) {
+  // Highlight button if it is the current page (or a child page)
   const router = useRouter();
-  const isActive = router.pathname === href;
+  const isActive =
+    href === "/dashboard"
+      ? href === router.pathname
+      : router.pathname.startsWith(href);
 
   return (
     <Link href={href} className={style.bounds} replace>
       <div className={style.symbolContainer}>
         <div className={isActive ? style.symbolAreaActive : style.symbolArea}>
-          <span className={style["material-symbols-outlined"]}>
-            {glyph}
-          </span>
+          <span className={style["material-symbols-outlined"]}>{glyph}</span>
         </div>
       </div>
       {label && <div className={style.label}>{label}</div>}
