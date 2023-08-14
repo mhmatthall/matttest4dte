5c19b48e76e20539a32bb88311f725257a615a91
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-08-14 20:00:27 +0100

    Fix Next.js bug to reset focus on layout refresh

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,8 @@ import "@/styles/globals.scss";
 import "material-symbols/outlined.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +12,14 @@ export default function App({ Component, pageProps }) {
   // We've got nested layouts, so to maintain the SPA-like navigation we have made each page define its own layout
   // Here we ask for the layout from the page, and fall back on no layout if the page doesn't define one
   const getLayout = Component.getLayout || ((page) => page);
+
+  // Fix for Next.js bug where element focus is not reset between page transitions
+  // See: https://github.com/vercel/next.js/issues/33060
+  const router = useRouter();
+  useEffect(() => {
+    document.activeElement instanceof HTMLElement &&
+      document.activeElement.blur();
+  }, [router]);
 
   return (
     <>
