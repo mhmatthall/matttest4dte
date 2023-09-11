c2141416355b9cb0fba5d5a63a2d41bfe56b4379
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-11 21:32:20 +0100

    Make Card component more reusable

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -4,6 +4,7 @@ export default function Card({
   headline,
   subhead,
   description,
+  children,
   actions,
   actionsDivider,
   actionsAlignment = "right",
@@ -14,6 +15,7 @@ export default function Card({
       {headline && <h1 className={style.headline}>{headline}</h1>}
       {subhead && <h2 className={style.subhead}>{subhead}</h2>}
       {description && <p className={style.description}>{description}</p>}
+      {children}
       {actionsDivider && <div className={style.actionsDivider} />}
       {actions && (
         <div className={style["actions-" + actionsAlignment]}>{actions}</div>
