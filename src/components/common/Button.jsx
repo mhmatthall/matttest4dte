beb5f7ca81481d09717d7d63583b9c33641080a3
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-11 21:31:19 +0100

    Make Button component more versatile

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,31 @@
 import Link from "next/link";
 import style from "./Button.module.scss";
 
-export default function Button({ href, label, variant }) {
-  return (
-    <Link href={href} className={style["bounds-" + variant]} replace>
-      <div className={style.buttonArea}>
-        <div className={style.label}>{label}</div>
-      </div>
-    </Link>
-  );
+export default function Button({ variant, label, href, ...props }) {
+  if (props.disabled) {
+    variant = "disabled";
+  }
+
+  if (href) {
+    return (
+      <Link
+        {...props}
+        href={href}
+        className={style["bounds-" + variant]}
+        replace
+      >
+        <div className={style.buttonArea}>
+          <div className={style.label}>{label}</div>
+        </div>
+      </Link>
+    );
+  } else {
+    return (
+      <button {...props} className={style["bounds-" + variant]}>
+        <div className={style.buttonArea}>
+          <div className={style.label}>{label}</div>
+        </div>
+      </button>
+    );
+  }
 }
