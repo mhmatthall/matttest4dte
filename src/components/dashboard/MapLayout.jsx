4464cc3b6511e26281f32c551cffb7c328ab582b
Fergus Pick <56115708+fergus-pick@users.noreply.github.com>
2023-11-20 16:16:47 +0000

    Add zoom and location controls to map

--- src/components/dashboard/MapLayout.jsx
+++ src/components/dashboard/MapLayout.jsx
@@ -1,5 +1,9 @@
 import "maplibre-gl/dist/maplibre-gl.css";
-import Map from "react-map-gl/maplibre";
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  NavigationControl,
+} from "react-map-gl/maplibre";
 import style from "./MapLayout.module.scss";
 
 export default function MapLayout({ children }) {
@@ -15,8 +19,13 @@ export default function MapLayout({ children }) {
             latitude: 51.61242,
             zoom: 16.0,
           }}
-        />
+        >
+          <GeolocateControl position="top-left" />
+          <FullscreenControl position="top-left" />
+          <NavigationControl position="top-left" />
+        </Map>
       </div>
+
       <div className={style.supportingPane}>{children}</div>
     </>
   );
